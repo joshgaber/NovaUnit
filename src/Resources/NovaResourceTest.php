560dbc47e5b80ed59c15faf41fce80161c60206f
Josh Gaber <joshgaber@gmail.com>
2020-06-16 00:01:43 -0400

    Allow Nova Resource test to be initialized without a model

--- src/Resources/NovaResourceTest.php
+++ src/Resources/NovaResourceTest.php
@@ -11,16 +11,18 @@ trait NovaResourceTest
      * Initialize the Nova Resource mock.
      *
      * @param string $class The class path of the Resource
-     * @param Model $model The object to apply to this Resource
+     * @param Model|null $model The object to apply to this Resource
      * @return MockResource The Resource mock instance
      * @throws InvalidNovaResourceException If the supplied action class is not a Nova Resource
      */
-    public static function novaResource(string $class, Model $model): MockResource
+    public static function novaResource(string $class, ?Model $model = null): MockResource
     {
         if (! \is_subclass_of($class, Resource::class)) {
             throw new InvalidNovaResourceException();
         }
 
-        return new MockResource(new $class($model));
+        $modelClass = get_class_vars($class)['model'];
+
+        return new MockResource(new $class($model ?? new $modelClass));
     }
 }
--- tests/Fixtures/Resources/ResourceValidFieldsAndActions.php
+++ tests/Fixtures/Resources/ResourceValidFieldsAndActions.php
@@ -5,12 +5,15 @@ namespace JoshGaber\NovaUnit\Tests\Fixtures\Resources;
 use Illuminate\Http\Request;
 use JoshGaber\NovaUnit\Tests\Fixtures\Actions\ActionNoFields;
 use JoshGaber\NovaUnit\Tests\Fixtures\Actions\ActionValidFields;
+use JoshGaber\NovaUnit\Tests\Fixtures\MockModel;
 use Laravel\Nova\Fields\Number;
 use Laravel\Nova\Fields\Text;
 use Laravel\Nova\Resource;
 
 class ResourceValidFieldsAndActions extends Resource
 {
+    public static $model = MockModel::class;
+
     public function fields(Request $request)
     {
         return [
