ebfe9323be45a259d7f6c191d32923a15c06e5ea
Josh Gaber <joshgaber@gmail.com>
2020-06-21 12:11:59 -0400

    Add tests for Action instances on components

---
+++ src/Actions/MockActionElement.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace JoshGaber\NovaUnit\Actions;
+
+use Laravel\Nova\Actions\Action;
+use PHPUnit\Framework\Assert as PHPUnit;
+
+class MockActionElement
+{
+    private $action;
+
+    public function __construct(Action $action)
+    {
+        $this->action = $action;
+    }
+
+    /**
+     * Assert that the action can be shown on the index view.
+     *
+     * @param string $message
+     * @return $this
+     */
+    public function assertShownOnIndex(string $message = ''): self
+    {
+        PHPUnit::assertTrue($this->action->showOnIndex, $message);
+
+        return $this;
+    }
+
+    /**
+     * Assert that the action is hidden from the index view.
+     *
+     * @param string $message
+     * @return $this
+     */
+    public function assertHiddenFromIndex(string $message = ''): self
+    {
+        PHPUnit::assertFalse($this->action->showOnIndex, $message);
+
+        return $this;
+    }
+
+    /**
+     * Assert that the action can be shown on the detail view.
+     *
+     * @param string $message
+     * @return $this
+     */
+    public function assertShownOnDetail(string $message = ''): self
+    {
+        PHPUnit::assertTrue($this->action->showOnDetail, $message);
+
+        return $this;
+    }
+
+    /**
+     * Assert that the action is hidden from the detail view.
+     *
+     * @param string $message
+     * @return $this
+     */
+    public function assertHiddenFromDetail(string $message = ''): self
+    {
+        PHPUnit::assertFalse($this->action->showOnDetail, $message);
+
+        return $this;
+    }
+
+    /**
+     * Assert that the action can be shown on table rows.
+     *
+     * @param string $message
+     * @return $this
+     */
+    public function assertShownOnTableRow(string $message = ''): self
+    {
+        PHPUnit::assertTrue($this->action->showOnTableRow, $message);
+
+        return $this;
+    }
+
+    /**
+     * Assert that the action is hidden from table rows.
+     *
+     * @param string $message
+     * @return $this
+     */
+    public function assertHiddenFromTableRow(string $message = ''): self
+    {
+        PHPUnit::assertFalse($this->action->showOnTableRow, $message);
+
+        return $this;
+    }
+}
